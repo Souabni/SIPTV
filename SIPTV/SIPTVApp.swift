b275d8db614f8c161c5398b7061422f75221f2c1
Issam Souabni <souabni.issam@gmail.com>
2022-08-24 17:36:53 +0200

    Initial Commit

---
+++ SIPTV/SIPTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIPTVApp.swift
+//  SIPTV
+//
+//  Created by Dev on 24/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SIPTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
